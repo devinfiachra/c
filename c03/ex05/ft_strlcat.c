878aded7aba0153912a15b0d15a46ca0bc5c02c0
Devin Fiachra <devinduffymeade@gmail.com>
2023-09-04 02:00:51 +0200

    solved strcmp

---
+++ c03/ex05/ft_strlcat.c
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/04 01:20:53 by marvin            #+#    #+#             */
+/*   Updated: 2023/09/04 01:20:53 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int ft_strlcat(char *dest, char *src, unsigned int size);
+
+unsigned int ft_strlcat(char *dest, char *src, unsigned int size)
+{
+}
